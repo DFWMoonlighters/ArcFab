108f89ecbdbfd967256783fca34d65cc6e0a7d24
vannvaughan <vann.vaughan@yahoo.com>
2017-10-03 17:35:11 -0400

    added bootstrap and jQuery links

--- public/app.js
+++ public/app.js
@@ -4,30 +4,37 @@ angular.module('app', ['ui.router'])
       .state('adminPortal', {
         url:'/adminPortal',
         templateUrl: "views/adminPortal.html",
+        controller: 'adminPortalCtrl'
       })
       .state('cart', {
         url:'/cart',
         templateUrl: "views/cart.html",
+        controller: 'cartCtrl'
       })
       .state('checkout', {
         url:'/checkout',
         templateUrl: "views/checkout.html",
+        controller: 'checkoutCtrl'
       })
       .state('landing', {
         url:'/landing',
         templateUrl: "views/landing.html",
+        controller: 'landingCtrl'
       })
       .state('productDetails', {
         url:'/productDetails',
         templateUrl: "views/productDetails.html",
+        controller: 'productDetailsCtrl'
       })
       .state('systemProducts', {
         url:'/systemProducts',
         templateUrl: "views/systemProducts.html",
+        controller: 'systemProductsCtrl'
       })
       .state('systemSelection', {
         url:'/systemSelection',
         templateUrl: "views/systemSelection.html",
+        controller: 'systemSelectionCtrl'
       })
       $urlRouterProvider
         .otherwise('/landing');
