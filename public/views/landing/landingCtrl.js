f9ea115537d3399ed99857019ce31dd3ce2a6edd
Daniel Hogeland <dhogeland@Daniels-MacBook-Pro.local>
2017-10-19 19:14:01 -0500

    added js-cookie script to index.html, js-cookie factory on app.js, generated cookie on landingCtrl, passed cookie with posted orders, pulling cookie to display connected orders on cart

--- public/views/landing/landingCtrl.js
+++ public/views/landing/landingCtrl.js
@@ -1,6 +1,9 @@
 angular.module('app')
   .controller("landingCtrl", function($scope, landingSrv) {
 
-
+    var cookie = Cookies.get('arcfab');
+    if (cookie === undefined) {
+      Cookies.set('arcfab', Math.random(), { expires: 7 })
+    }
 
   });
