0651c569ffeb0cacddbec2c3615ab26ee7e6c99b
vannvaughan <vann.vaughan@yahoo.com>
2017-11-13 14:13:10 -0600

    removed console.logs

--- public/views/systemProducts/systemProductsCtrl.js
+++ public/views/systemProducts/systemProductsCtrl.js
@@ -1,17 +1,15 @@
 angular.module('app')
   .controller("systemProductsCtrl", function($scope, systemProductsSrv, $stateParams) {
 
-    console.log( "systemProductsCtrl stateParams:", $stateParams )
-
     let system = $stateParams.system
-    console.log("systemProductCtrl system:", system)
+
     systemProductsSrv.getProducts( system )
       .then( res => {
-        console.log( res ) 
+        console.log( "getProducts response:", res ) 
         $scope.products = res.data
       })
       .catch( err => {
-        console.log(err)
+        console.log( "getProducts error:", err)
         return err
       })
 
--- public/views/systemProducts/systemProductsService.js
+++ public/views/systemProducts/systemProductsService.js
@@ -2,7 +2,6 @@ angular.module('app')
   .service('systemProductsSrv', function( $http ) {
 
     this.getProducts = ( system ) => {
-      console.log("systemProductSrv system:", system)
       return $http.get('http://localhost:3001/api/products/' + system)
     }
 
